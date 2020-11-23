fc3317b635d84c2f37e8bbbec55d62516071fd55
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-23 12:41:53 -0500

    add delete url route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const ShortUrl = require("./models/shortUrl");
 const app = express();
 var bodyParser = require("body-parser");
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({extended: false}));
+//override with post having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 
 //Routes
@@ -24,21 +27,21 @@ app.get('/', async (req, res) => {
     res.render("index", {shortUrls: shortUrls});
 });
 
-
+//new url form route
 app.get("/newUrl", (req, res) => {
     res.render("newUrl");
 });
 
-
+//add short url route
 app.post("/shortUrls", async (req, res) => {
     await ShortUrl.create({base: req.body.url});
     res.redirect("/");
 });
 
+//short url match
 app.get("/:shortUrl", async (req, res) => {
     //find short url based on url param
     const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl});
-    console.log("Short Url: " + req.params.shortUrl);
     if(!shortUrl) //url not found
         return res.sendStatus(404);
 
@@ -47,7 +50,20 @@ app.get("/:shortUrl", async (req, res) => {
 
     //redirect to original url
     res.redirect(shortUrl.base);
-})
+});
+
+//url delete route
+app.delete("/:shortUrl", async (req, res) => {
+    await ShortUrl.deleteOne({short: req.params.shortUrl}, (err) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+
 
 
 const port = process.env.PORT || 4000;
